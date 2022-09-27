6ca4e18cde017470d8751832cf13ecf6f2bf4cab
Meikaji <2024dcasillas@doradoacademy.org>
2022-09-27 09:04:32 -0400

    Git interface test

--- guessnum.py
+++ guessnum.py
@@ -1,3 +1,9 @@
-x = "chicken"
-y = 7
-print(x)
+z = input("Guess the Number: ")
+import random
+randnum = random.randrange(1,9)
+print (z)
+print (randnum)
+if z == randnum :
+    print("You Win.")
+else :
+    print ("You lose.")
